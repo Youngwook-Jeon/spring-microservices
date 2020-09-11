bc6887047bf521687f77f26da54d17b5239662a8
Young wook Jeon <ela87@naver.com>
2020-09-11 14:47:43 +0900

    Add H2 database dependency and user model.

--- photo-app-api-users/src/main/java/com/example/photoappapiusers/users/ui/controllers/UsersController.java
+++ photo-app-api-users/src/main/java/com/example/photoappapiusers/users/ui/controllers/UsersController.java
@@ -1,15 +1,24 @@
 package com.example.photoappapiusers.users.ui.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.photoappapiusers.users.ui.models.CreateUserRequestModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working";
+        return "Working on port " + env.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody CreateUserRequestModel userRequestModel) {
+        return "Create user method is called.";
     }
 }
