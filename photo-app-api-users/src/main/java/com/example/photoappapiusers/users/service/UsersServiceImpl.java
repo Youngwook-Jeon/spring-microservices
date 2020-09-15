3c2c95dcdeb5210c01548905d7d26978423b0f99
Young wook Jeon <ela87@naver.com>
2020-09-15 14:13:57 +0900

    Add Spring Security dependencies

--- photo-app-api-users/src/main/java/com/example/photoappapiusers/users/service/UsersServiceImpl.java
+++ photo-app-api-users/src/main/java/com/example/photoappapiusers/users/service/UsersServiceImpl.java
@@ -1,14 +1,34 @@
 package com.example.photoappapiusers.users.service;
 
+import com.example.photoappapiusers.users.data.UserEntity;
+import com.example.photoappapiusers.users.data.UsersRepository;
 import com.example.photoappapiusers.users.shared.UserDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class UsersServiceImpl implements UsersService {
+
+    UsersRepository usersRepository;
+
+    @Autowired
+    public UsersServiceImpl(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
 
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
-        return null;
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
+        userEntity.setEncryptedPassword("test");
+        usersRepository.save(userEntity);
+
+        return modelMapper.map(userEntity, UserDto.class);
     }
 }
--- photo-app-api-users/src/main/java/com/example/photoappapiusers/users/ui/controllers/UsersController.java
+++ photo-app-api-users/src/main/java/com/example/photoappapiusers/users/ui/controllers/UsersController.java
@@ -1,8 +1,15 @@
 package com.example.photoappapiusers.users.ui.controllers;
 
+import com.example.photoappapiusers.users.service.UsersService;
+import com.example.photoappapiusers.users.shared.UserDto;
 import com.example.photoappapiusers.users.ui.models.CreateUserRequestModel;
+import com.example.photoappapiusers.users.ui.models.CreateUserResponseModel;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -14,13 +21,22 @@ public class UsersController {
     @Autowired
     private Environment env;
 
+    @Autowired
+    UsersService usersService;
+
     @GetMapping("/status/check")
     public String status() {
         return "Working on port " + env.getProperty("local.server.port");
     }
 
     @PostMapping
-    public String createUser(@Valid @RequestBody CreateUserRequestModel userRequestModel) {
-        return "Create user method is called.";
+    public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userRequestModel) {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDto userDto = modelMapper.map(userRequestModel, UserDto.class);
+        UserDto createdUser = usersService.createUser(userDto);
+        CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
     }
 }
